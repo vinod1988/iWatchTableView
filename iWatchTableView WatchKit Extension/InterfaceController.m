2b962c7a21be3b377ae75af7ba8d79082618ebc2
Vinod Vishwakarma <vinod.vishwakarma@raweng.com>
2015-03-25 18:00:38 +0530

    # First commit

---
+++ iWatchTableView WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  iWatchTableView WatchKit Extension
+//
+//  Created by Vinod Vishwakarma on 18/03/15.
+//  Copyright (c) 2015 Vinod Vishwakarma. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
---
+++ iWatchTableView WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  iWatchTableView WatchKit Extension
+//
+//  Created by Vinod Vishwakarma on 18/03/15.
+//  Copyright (c) 2015 Vinod Vishwakarma. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
